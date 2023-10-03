1cbae1f295088e46ccc7b730f81dc4d418ae5c50
saiminh <saiminh@gmail.com>
2023-10-03 17:34:40 +0200

    honeypot?

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -72,7 +72,7 @@
 </script>
 
 <div class="formwrapper">
-  <form name="contact" action="/success" method="POST" data-netlify="true" >
+  <form name="contact" action="/success" method="POST" data-netlify="true" { ...{ 'netlify-honeypot': 'bot-field' } }>
     <input type="hidden" name="form-name" value="contact">
     <section class="contact">
       <h1 class="toCanvas">Don't be a stranger. Let's get acquainted.</h1>
