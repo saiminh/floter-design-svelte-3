e35b84188e995fe9e470f724b1e77f4345dc21f4
saiminh <saiminh@gmail.com>
2023-10-20 09:39:02 +0200

    remove imagetools formats

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-const supportedExtensions = ['png', 'jpg', 'jpeg'];
-
 export default defineConfig({
 	plugins: [ sveltekit() ]
 });
