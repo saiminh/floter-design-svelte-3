dec953a6c1558625dddda3f841ebda1ee474a51c
saiminh <saiminh@gmail.com>
2023-10-20 09:38:28 +0200

    fix dependencies warning, remove imagetools

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { imagetools } from 'vite-imagetools';
 import { defineConfig } from 'vite';
 
 const supportedExtensions = ['png', 'jpg', 'jpeg'];
 
 export default defineConfig({
-	plugins: [
-    imagetools({
-      defaultDirectives: (url) => {
-        const extension = url.pathname.substring(
-          url.pathname.lastIndexOf('.') + 1
-        );
-        if (supportedExtensions.includes(extension)) {
-          return new URLSearchParams({
-            format: 'avif;webp;' + extension,
-            // picture: true
-          });
-        }
-        return new URLSearchParams();
-      }
-    }),
-    sveltekit()]
+	plugins: [ sveltekit() ]
 });
