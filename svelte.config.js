36defd0b570505d69e58cc0bfd5c88f2d0b4a88b
saiminh <saiminh@gmail.com>
2023-10-30 17:07:11 +0100

    csp tweaks

--- svelte.config.js
+++ svelte.config.js
@@ -25,20 +25,17 @@ const config = {
       // if true, will create a Netlify Edge Function rather
       // than using standard Node-based functions
       edge: false,
- 
       // if true, will split your app into multiple functions
       // instead of creating a single one for the entire app.
       // if `edge` is true, this option cannot be used
       split: false
     }),
     csp: {
+      mode: 'auto',
       directives: {
-        'script-src': ['self', '*.typekit.net', '*.cloudinary.com']
+        'script-src': ['self', 'unsafe-eval']
       },
-      reportOnly: {
-        'script-src': ['self', '*.typekit.net', '*.cloudinary.com']
-      }
-    }
+    },
     // prerender: {
     //   force: true
     // }
